f4e56aba32e37346bf135b30794e7e524ec956f3
Demon.Lee <jwkljh@gmail.com>
2023-04-11 15:43:35 +0800

    modify github action cmd

--- ch16/cmd.sh
+++ ch16/cmd.sh
@@ -4,6 +4,8 @@ git clone https://github.com/lyzhang1999/kubernetes-example.git
 # add github-workflows and push to my github repository
 git remote set-url origin git@github.com:LeoDemon/kubernetes-github-action-eg.git
 
+# modify `.github/workflows/build.yaml`
+
 # create docker hub secret
 
 # create github action secret
