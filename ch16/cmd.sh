6afc8e3538f43a8158d6cd7563bd2bedf18a30e3
Demon.Lee <jwkljh@gmail.com>
2023-04-11 13:59:08 +0800

    github action workflow practice

---
+++ ch16/cmd.sh
@@ -0,0 +1,14 @@
+# clone example manifests
+git clone https://github.com/lyzhang1999/kubernetes-example.git
+
+# add github-workflows and push to my github repository
+git remote set-url origin git@github.com:LeoDemon/kubernetes-github-action-eg.git
+
+# create docker hub secret
+
+# create github action secret
+
+# trigger github action workflow
+git commit --allow-empty -m "Trigger Build"
+git push origin main
+
